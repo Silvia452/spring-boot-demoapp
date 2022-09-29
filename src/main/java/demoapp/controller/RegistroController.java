dc8ba34836711d4a393bd5abf82b69d45f72cf42
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2022-09-29 12:42:23 +0200

    Nuevo servicio pero sin funcionar

--- src/main/java/demoapp/controller/RegistroController.java
+++ src/main/java/demoapp/controller/RegistroController.java
@@ -1,24 +1,29 @@
 package demoapp.controller;
+
+import demoapp.service.RegistroService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
 
 public class RegistroController {
 
-    @RequestMapping("/form")
+    @Autowired
+    private RegistroService service;
+
+    @RequestMapping("/user/{nombre}")
     public String showUserForm(Model model){
         model.addAttribute("usuario", new Usuario());
         return "userForm";
     }
-
+    public @ResponseBody String usuario(@PathVariable(value="nombre") String nombre) {
+        return service.saluda(nombre);
+    }
 
 
 }
-
-
-
-
